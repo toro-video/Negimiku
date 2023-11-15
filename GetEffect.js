2e0bc75284610a28dae3e4bccada3ca7d4ba2890
toro-video <80771549+toro-video@users.noreply.github.com>
2023-11-15 10:39:36 +0900

    Update GetEffect.js

--- GetEffect.js
+++ GetEffect.js
@@ -4,20 +4,20 @@ let millis_time;//How long to display the effect. -> "getLeek" method
 
 class GetEffect{
 	
-	//getLeek_effect
-	getLeekEffect(){
+    //getLeek_effect
+    getLeekEffect(){
 		
-		if(score > c){//If the score increases, reserve a variable　"t".
-			cnt = t;
-		}
+	if(score > c){//If the score increases, reserve a variable　"t".
+		cnt = t;
+	}
 		
-		/*
-		When the difference between the obtained variable 'cnt' and 't'
-		 is less than or equal to the variable "millis_time"
-		And cnt is not 0 (not the initial value), display the image "get".
-		*/
-		if(cnt - t < millis_time  &&  cnt != 0){
-			image(img_get, x, y - height/9, 80, 27.2);
-		}
-	} 
+	/*
+ 	When the difference between the obtained variable 'cnt' and 't'
+	is less than or equal to the variable "millis_time"
+	And cnt is not 0 (not the initial value), display the image "get".
+	*/
+	if(cnt - t < millis_time  &&  cnt != 0){
+		image(img_get, x, y - height/9, 80, 27.2);
+	}
+    } 
 }
